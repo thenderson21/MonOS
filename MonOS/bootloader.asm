a08a36db4a3963a5c06094dd79dd77e3a00d2b6f
Todd <thenderson21@users.noreply.github.com>
2016-04-12 17:35:28 -0500

    Update bootloader.asm

--- MonOS/bootloader.asm
+++ MonOS/bootloader.asm
@@ -1,41 +1,49 @@
-BITS 16
+[org 0x7c00]
+KERNEL_OFFSET equ 0x1000 ; The same one we used when linking the kernel
 
-start:
-	; Set up 4K stack after this bootloader
-	; [Remember: Effective Address = Segment*16 + Offset]
-	mov ax, 07C0h   ; Set 'ax' equal to the location of this bootloader divided by 16
-	add ax, 20h     ; Skip over the size of the bootloader divided by 16
-	mov ss, ax      ; Set 'ss' to this location (the beginning of our stack region)
-	mov sp, 4096    ; Set 'ss:sp' to the top of our 4K stack
+    mov [BOOT_DRIVE], dl ; Remember that the BIOS sets us the boot drive in 'dl' on boot
+    mov bp, 0x9000
+    mov sp, bp
 
-	; Set data segment to where we're loaded so we can implicitly access all 64K from here
-	mov ax, 07C0h   ; Set 'ax' equal to the location of this bootloader divided by 16
-	mov ds, ax      ; Set 'ds' to the this location
+    mov bx, MSG_REAL_MODE 
+    call print
+    call print_nl
 
-	; Print our message and stop execution
-	mov si, message ; Put address of the null-terminated string to output into 'si'
-	call print      ; Call our string-printing routine
-	cli             ; Clear the Interrupt Flag (disable external interrupts)
-	hlt             ; Halt the CPU (until the next external interrupt)
+    call load_kernel ; read the kernel from disk
+    call switch_to_pm ; disable interrupts, load GDT,  etc. Finally jumps to 'BEGIN_PM'
+    jmp $ ; Never executed
+
+%include "print.asm"
+%include "print_hex.asm"
+%include "disk.asm"
+%include "32bit-gdt.asm"
+%include "32bit-print.asm"
+%include "32bit-switch.asm"
+
+[bits 16]
+load_kernel:
+    mov bx, MSG_LOAD_KERNEL
+    call print
+    call print_nl
+
+    mov bx, KERNEL_OFFSET ; Read from disk and store in 0x1000
+    mov dh, 2
+    mov dl, [BOOT_DRIVE]
+    call disk_load
+    ret
+
+[bits 32]
+BEGIN_PM:
+    mov ebx, MSG_PROT_MODE
+    call print_string_pm
+    call KERNEL_OFFSET ; Give control to the kernel
+    jmp $ ; Stay here when the kernel returns control to us (if ever)
 
 
-data:
-	message db 'Loading kernel...', 0
-
-
-; Routine for outputting string in 'si' register to screen
-print:
-	mov ah, 0Eh     ; Specify 'int 10h' 'teletype output' function
-	                ; [AL = Character, BH = Page Number, BL = Colour (in graphics mode)]
-.printchar:
-	lodsb           ; Load byte at address SI into AL, and increment SI
-	cmp al, 0
-	je .done        ; If the character is zero (NUL), stop writing the string
-	int 10h         ; Otherwise, print the character via 'int 10h'
-	jmp .printchar  ; Repeat for the next character
-.done:
-	ret
-
+BOOT_DRIVE db 0 ; It is a good idea to store it in memory because 'dl' may get overwritten
+MSG_REAL_MODE db "Started in 16-bit Real Mode", 0
+MSG_PROT_MODE db "Landed in 32-bit Protected Mode", 0
+MSG_LOAD_KERNEL db "Loading kernel into memory", 0
 
 ; Pad to 510 bytes (boot sector size minus 2) with 0s, and finish with the two-byte standard boot signature
 times 510-($-$$) db 0
